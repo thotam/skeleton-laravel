fcc2550a70105ecc29b83cc9dc474ffbc4489b0b
thotam <thanhtamtqno1@gmail.com>
2023-05-04 13:39:06 +0700

    Update routes.php

--- routes/routes.php
+++ routes/routes.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['web','auth', 'CheckAccount', 'CheckHr', 'CheckInfo'])->group(function () {
-
+Route::middleware(['web', 'auth', 'CheckAccountAndHr'])->group(function () {
 });
